4b435384a142d18eaba7d0e097f08193a8ee9b5d
minhazapon <minhajulabedinapon@gmail.com>
2024-04-26 04:48:16 -0700

    animate files

--- src/Cinformation.jsx
+++ src/Cinformation.jsx
@@ -5,7 +5,7 @@ import 'animate.css';
 
 const Cinformation = () => {
     return (
-        <div className=" mb-10 mt-10">
+        <div className="  mb-32 mt-10">
 
             <div>
 
@@ -18,23 +18,30 @@ const Cinformation = () => {
 
                 </div>
 
-
+               <div className=' flex justify-center items-center gap-20 mt-10'>
 
 
                 <div className=' animate__animated animate__flip '>
-
                    <img className=' border-[1px] border-secondary rounded-xl animate__animated animate__rollIn  h-[200px] w-[300px]'
                     src="https://img.freepik.com/free-photo/person-scrapbooking-home_23-2149614045.jpg?t=st=1714127689~exp=1714131289~hmac=c8d4cfe093ec17f4c438db70186e7eae97ff8e22ac4e1945b7b4f0f9b47c8c96&w=740 " alt="" />
-
                   <p className=' mt-1 mb-1 text-xl'>A craft or trade is a pastime <br></br>or an occupation that requires<br></br> particular skills<br></br>
                      and knowledge of skilled work. <br></br> In a historical sense, particularly <br></br>the Middle Ages and earlier</p>
-
-                    <div className=' mt-2 text-white rounded-xl p-3 w-[300px] text-xl bg-secondary '>
+                    <div className=' mt-2 text-white rounded-xl p-3 w-[300px] text-xl  bg-blue-600 '>
                         <h1>A handicraft, sometimes more <br></br> precisely expressed as artisanal <br></br> handicraft or handmade, <br></br> is any of a wide <br></br> variety of types of work where <br></br> useful and decorative</h1>
-
                     </div>
+                </div>
 
 
+
+                
+                <div className=' -mb-20 animate__animated animate__flip '>
+                <div className=' mt-2 text-white rounded-xl p-3 w-[300px] text-xl bg-violet-800  '>
+                        <h1>Arts and crafts describes a wide variety of activities involving making things with one's own hands. Arts and crafts is usually a hobby. Some crafts (art skills) have been practised since preshistoric times</h1>
+                    </div>
+                  <p className=' mt-1 mb-1 text-xl'>Historically, the more specialized<br></br> crafts with high-value products <br></br> tended to concentrate <br></br>in urban centers and <br></br> their practitioners<br></br> formed guilds. </p>
+                     <img className=' border-[1px] border-secondary rounded-xl animate__animated animate__rollIn  h-[200px] w-[300px]'
+                    src="  https://img.freepik.com/free-photo/vibrant-summer-picnic-table-with-colorful-decorations-generated-by-ai_188544-54137.jpg?t=st=1714131432~exp=1714135032~hmac=6ae018560697c881a0a757ddd6a703535fda420520d05c9baec6e125cf28bf8e&w=826   " alt="" />
+                </div>
                 </div>
 
 
